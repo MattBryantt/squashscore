1b0a0aa21fe0ab48411fb9121326080f626c0cf6
Matt <u7496235@anu.edu.au>
2024-10-26 11:23:42 +1100

    Initalised score counters and serve button.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,15 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen 
+      name="index"
+      options={{
+        title: 'SquashScore',
+        headerStyle: {
+          backgroundColor: "#1f1f1f"
+        },
+        headerTintColor: "#f1f1f1"
+      }} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,119 @@
 import React, { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, View, StyleSheet} from "react-native";
+
+
 
 export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.scorePanelContainer}>
+        <ScorePanel/>
+      </View>
+      <View style={styles.otherContainer}>
+        <ScoreCard/>
+      </View>
+    </View>
+  );
+};
+
+const ScorePanel = () => {
+  return (
+    <View style={styles.scorePanelContainer}>
+      <View style={styles.scoreContainer}>
+        <Score/>
+        <Text style={styles.scoreText}>-</Text>
+        <Score/>
+      </View>
+      <ServeSymbol/>
+    </View>
+  );
+};
+
+const Score = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>
-        Edit app/index.tsx to edit this screen.
-      </Text>
-      <Pressable onPress={() => setCount(count + 1)}>
-        <Text>{count}</Text>
-      </Pressable>
+    <Pressable onPress={() => setCount(count + 1)}>
+      <Text style={styles.scoreText}>{count}</Text>
+    </Pressable>
+  );
+};
+
+const ServeSymbol = () => {
+  const [serveLeft, setServeLeft] = useState(true);
+
+  const togglePosition = () => {
+    setServeLeft(!serveLeft);
+  };
+  return (
+    <Pressable
+      onPress={togglePosition}
+      style={[styles.serveButton, 
+        serveLeft ? styles.serveLeftButton : styles.serveRightButton]}
+    />
+  );
+};
+
+const ScoreCard = () => {
+  return (
+    <View>
+      <Text style={styles.scoreCardText}>Scorecard</Text>
     </View>
   );
-}
+};
+
+// Styles
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1f1f1f"
+  },
+  scorePanelContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "90%",
+    paddingBottom: 15,
+  },
+  otherContainer: {
+    flex: 2,
+    width: "90%",
+    paddingTop: 15,
+  },
+
+  scoreContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "40%",
+  },
+  scoreText: {
+    fontSize: 52,
+    color: "#f1f1f1",
+    fontWeight: "bold"
+  },
+
+  serveButton: {
+    position: "absolute",
+    backgroundColor: "#f1f1f1",
+    width: 22,
+    height: 22,
+    borderRadius: 11,
+    bottom: 0,
+    padding: 10
+  },
+  serveLeftButton: {
+    left: 0,
+  },
+  serveRightButton: {
+    right: 0,
+  },
+
+  scoreCardText: {
+    fontSize: 26,
+    color: "#f1f1f1",
+    fontWeight: "bold",
+    textAlign: "left",
+  }
+});
